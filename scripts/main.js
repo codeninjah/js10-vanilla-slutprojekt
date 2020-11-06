2d31c9a25fcb710244bd0b92ce4432427de1081c
codeninjah <codeninjahblog@gmail.com>
2020-11-06 15:38:19 +0100

    Småfix

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,11 @@
 console.log("It works!")
-//test
-
-/*
-if(document.querySelector(".beer-pic").innerText.length == 0) {
-    document.querySelector(".beer-card").classList.add(".hidden")
-}
-*/
 
 //-------------------- FETCHING API ---------------//
-
 async function getRandomBeer(rndNr) {
     const req = await fetch ("https://api.punkapi.com/v2/beers/" + rndNr) 
     const res = await req.json()
     return res
 };
-
 
 //--------------------PRINT FUNCTIONEN -------------//
 async function print() {
@@ -22,25 +13,28 @@ let randomNr = Math.floor(Math.random() * 100);     // returns a random integer 
 
 let result = await getRandomBeer(randomNr) // Anropar getRandomBeer functionen och skickar med ett randomNr som parameter
 
-
 console.log(result) //JUST TO TEST
-
-//document.querySelector(".beer-card").classList.remove(".hidden")
-
-document.querySelector(".beer-pic").innerHTML = ""
 
 document.querySelector(".beer-pic").innerHTML = "<img src='" + result[0].image_url + "'/>"
 document.querySelector(".beer-name").innerText = result[0].name
 
-document.querySelector("#beer-info").innerHTML = "<p>" + result[0].description + "</p>"
-document.querySelector("#beer-info").classList.add("hidden")
+const binfo = document.querySelector("#beer-info")
 
+binfo.innerHTML = "<p>" + result[0].description + "</p>"
+binfo.innerHTML += "<p>Alcohol by volume: " + result[0].abv + "</p>"
+binfo.innerHTML += "<p>Volume: " + result[0].volume.value + " " + result[0].volume.unit
+//binfo.innerHTML += //Ingredients
+//binfo.innerHTML += //Hops
+//binfo.innerHTML += //Food pairing
+binfo.innerHTML += "<p>Brewer tips: " + result[0].brewers_tips + "</p>"
+
+binfo.classList.add("hidden")
 }
+//---------------------END FUNKTIONEN PRINT -------------------//
 
 
 // VILL DU LADDA IN EN RANDOM BEER, ANVÄND BORTKOMMENTERADE FUNKTIONSANROPET NEDAN
 //print()
-
 
 const randomiseBtn = document.querySelector(".random-beer-btn")
 randomiseBtn.addEventListener("click", function(){
@@ -51,16 +45,8 @@ randomiseBtn.addEventListener("click", function(){
 //------------------ EXEMPEL PÅ HUR DESCRIPTION SKULLE KUNNA FUNKA ----------//
 const bInfo = document.querySelector(".info")
 const bInfoText = document.querySelector("#beer-info")
+
 bInfo.addEventListener("click", function(){
-
-    /*
-    bInfoText.innerHTML = "<h1>Beer Info</h1>" + 
-    "<p>Testing some stuff. This beer was manufactured back then</p>" +
-    "<p>And it's good ol' information to know. Why am I writing this in English?" +
-        "I don't know but I guees I wanna try some stuff and see where this leads. Truely awesome." +
-    "You're breathtaking! No, YOU are breathtaking! :D</p>"
-    */
-
     bInfoText.classList.remove("hidden")
 });
 
